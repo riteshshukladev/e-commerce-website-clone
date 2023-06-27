f5a015acc9b2e4e00eda7b2152b70b1213c69e36
riteshshukladev <108568521+riteshshukladev@users.noreply.github.com>
2023-06-27 20:30:20 +0530

    Added some more features to single product page

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ if(close){
         nav.classList.remove("active");
     })
 }
+
+// Single Product's Image Slider
+
+const mainImg = document.querySelector('.main-img');
+const smallImgs = document.querySelectorAll('.small-img');
+
+smallImgs.forEach((smallImg) => {
+  smallImg.addEventListener('click', () => {
+    // Remove the active class from all the small images
+    smallImgs.forEach((img) => {
+      img.classList.remove('active_img');
+    });
+
+    // Set the clicked small image as active
+    smallImg.classList.add('active_img');
+
+    // Set the clicked small image as the source of the main image
+    mainImg.src = smallImg.src;
+  });
+});
+
+// Single Product's Image Slider End
+
+// Linking the product's image to the product page
+
+const prods = document.querySelectorAll('.prods');
+
+prods.forEach((prod) => {
+    prod.addEventListener('click', () => {
+        window.location.href = "sproduct.html";
+    }
+    );
+});
+
+
+
+
+
